09e7dd18a43d72603f8aa30d6efb32b274c7a7e9
AndrewIzatt <izatt.andrew@gmail.com>
2024-08-02 21:53:12 -0600

    Update aliases and functions to be compatible with fish

--- config.fish
+++ config.fish
@@ -1,17 +1,16 @@
-source ~/bash_config/.bash_aliases
-
 ### ADDING TO THE PATH
 # First line removes the path; second line sets it.  Without the first line,
 # your path gets massive and fish becomes very slow.
 # set -e fish_user_paths
 # set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications /var/lib/fla
 # set -x NVIM_APPNAME nvim-kickstart
+# set -x NVIM_APPNAME nvim-custom
 fish_default_key_bindings
-set -x NVIM_APPNAME nvim-custom
+set -x NVIM_APPNAME nvim
 set -g theme_nerd_fonts yes
 set -g fish_term24bit 1
 ### EXPORT ###
-set fish_greeting                                 # Supresses fish's intro message
+set fish_greeting # Suppresses fish's intro message
 
 ### SET MANPAGER
 ### Uncomment only one of these!
@@ -22,6 +21,12 @@ set fish_greeting                                 # Supresses fish's intro messa
 ### "less" as manpager
 set -x MANPAGER "less"
 
+if test -f ~/bash_config/.bash_aliases
+    source ~/bash_config/.bash_aliases
+end
 
+if test -f ~/.config/fish/functions.fish
+    source ~/.config/fish/functions.fish
+end
 
 fastfetch
